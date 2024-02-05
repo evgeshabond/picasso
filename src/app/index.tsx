e6dbf408d01fc458f0221c25972e4185a66ece5e
bondarenko-scalara <e.bondarenko@scalara.de>
2024-02-05 22:44:44 +0300

    basic app configuration

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Routing } from "../pages/Routing";
+import "./index.css";
+import { withProviders } from "./providers/with-providers";
+
+function App() {
+  return <Routing />;
+}
+
+const WithProviders = withProviders(App);
+
+export { WithProviders as App };
