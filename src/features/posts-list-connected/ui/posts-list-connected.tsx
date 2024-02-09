29bfd3e60ea11404815af273d0984426ccca8ce3
bondarenko-scalara <e.bondarenko@scalara.de>
2024-02-09 21:44:34 +0300

    make infinite scroll work properly

--- src/features/posts-list-connected/ui/posts-list-connected.tsx
+++ src/features/posts-list-connected/ui/posts-list-connected.tsx
@@ -1,28 +1,31 @@
 import { useVirtualizer } from "@tanstack/react-virtual";
 import { Post, postsApi } from "entities";
-import { debounce } from "lodash";
 import { useEffect, useRef, useState } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
 export const PostsListConnected = () => {
   const parentRef = useRef<HTMLDivElement>(null);
   const [page, setPage] = useState(0);
-  const { data } = postsApi.useGetPostsQuery(page);
+  const { data, isLoading, isFetching } = postsApi.useGetPostsByPageQuery(page);
+  const dispatch = useDispatch();
 
-  useEffect(() => console.log({ posts: data?.posts }), [data?.posts]);
-
-  // const count = data?.posts ? (data.hasNextPage ? data.posts.length + 1 : data.posts.length) : 0;
+  //  reset cache when component unmounts
+  useEffect(() => {
+    return () => {
+      dispatch(postsApi.util.resetApiState());
+    };
+  }, []);
 
   const rowVirtualizer = useVirtualizer({
-    count: data ? (data.hasNextPage ? data.posts.length + 1 : data.posts.length) : 0,
+    count: data ? data.posts.length : 0,
     getScrollElement: () => parentRef.current,
     estimateSize: () => 35,
   });
 
-  const increasePageDebounced = debounce(() => {
-    console.log("increasing page");
+  const increasePage = () => {
     setPage((p) => p + 1);
-  });
+  };
 
   useEffect(() => {
     const [lastItem] = [...rowVirtualizer.getVirtualItems()].reverse();
@@ -31,16 +34,17 @@ export const PostsListConnected = () => {
       return;
     }
 
-    if (lastItem.index >= data.posts.length - 1 && data.hasNextPage) {
-      increasePageDebounced();
+    if (lastItem.index >= data.posts.length - 1 && data.hasNextPage && !isFetching && !isLoading) {
+      increasePage();
     }
-  }, [data, rowVirtualizer.getVirtualItems()]);
+  }, [data, isLoading, rowVirtualizer.getVirtualItems(), isFetching, isLoading]);
 
   const getPost = (index: number) => (data?.posts ? data.posts[index] : undefined);
-  console.log({ posts: data?.posts });
+
+  const renderLoading = <div>Идет загрузка</div>;
 
   return (
-    <>
+    <div>
       <div
         ref={parentRef}
         style={{
@@ -50,32 +54,31 @@ export const PostsListConnected = () => {
         }}
       >
         <div
+          className="w-full relative"
           style={{
             height: `${rowVirtualizer.getTotalSize()}px`,
-            width: "100%",
-            position: "relative",
           }}
         >
-          {rowVirtualizer.getVirtualItems().map((virtualItem) => (
-            <div
-              key={virtualItem.key}
-              style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: "100%",
-                height: `${virtualItem.size}px`,
-                transform: `translateY(${virtualItem.start}px)`,
-              }}
-            >
-              <div key={virtualItem.key} className="flex justify-between max-w-full">
-                <Post post={getPost(virtualItem.index)} />
-                {getPost(virtualItem.index) && <Link to={`/post/${getPost(virtualItem.index)!.id}`}>Просмотр</Link>}
-              </div>
-            </div>
-          ))}
+          {isLoading
+            ? renderLoading
+            : rowVirtualizer.getVirtualItems().map((virtualItem) => (
+                <div
+                  className="w-full left-0 top-0 absolute"
+                  key={virtualItem.key}
+                  style={{
+                    height: `${virtualItem.size}px`,
+                    transform: `translateY(${virtualItem.start}px)`,
+                  }}
+                >
+                  <div key={virtualItem.key} className="flex justify-between max-w-full">
+                    <Post post={getPost(virtualItem.index)} />
+                    {getPost(virtualItem.index) && <Link to={`/post/${getPost(virtualItem.index)!.id}`}>Просмотр</Link>}
+                  </div>
+                </div>
+              ))}
         </div>
       </div>
-    </>
+      <div>{!isLoading && isFetching && renderLoading}</div>
+    </div>
   );
 };
