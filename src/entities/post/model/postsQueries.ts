225bb3b8d03d671d3a36bee3eb0a8c58a1d760ef
bondarenko-scalara <e.bondarenko@scalara.de>
2024-02-09 12:10:23 +0300

    wip: working infinite scroll, buth caching bug

--- src/entities/post/model/postsQueries.ts
+++ src/entities/post/model/postsQueries.ts
@@ -5,19 +5,73 @@ import { PostDto } from "./postDto";
 
 const POSTS_URL = BASE_URL + "/posts";
 
+//  constants required to mock
+const POSTS_COUNT = 100;
+const POSTS_ON_PAGE = 10;
+
+// const getHasNextPage = (page: number) => {
+//   return ;
+// };
+
 // Define a service using a base URL and expected endpoints
 export const postsApi = createApi({
   reducerPath: "postsApi",
   baseQuery: fetchBaseQuery({ baseUrl: POSTS_URL }),
   endpoints: (builder) => ({
-    getPosts: builder.query<PostDto[], void>({
-      query: () => `/`,
+    getPosts: builder.query<{ posts: PostDto[]; hasNextPage: boolean }, number>({
+      query: () => "",
+      transformResponse: (posts: PostDto[], meta, page) => {
+        const firstPostIndex = page ? page * POSTS_ON_PAGE : 0;
+        const lastPostIndex = firstPostIndex + 10;
+        const hasNextPage = POSTS_COUNT > POSTS_ON_PAGE * page;
+
+        const postsOnPage = posts.slice(firstPostIndex, lastPostIndex);
+        console.log("transforming response", { posts, postsOnPage, hasNextPage, firstPostIndex, lastPostIndex });
+        return { posts: postsOnPage, hasNextPage };
+      },
+      serializeQueryArgs: ({ endpointName }) => {
+        return endpointName;
+      },
+      // queryFn: async (page) => {
+      //   try {
+      //     console.log("doing call");
+      //     const posts: PostDto[] = await (await fetch(POSTS_URL)).json();
+      //     console.log(posts);
+      //     const firstPostIndex = page * POSTS_ON_PAGE - 1;
+      //     const lastPostIndex = firstPostIndex + 10;
+      //     const hasNextPage = POSTS_COUNT > POSTS_ON_PAGE * page;
+
+      //     const postsOnPage = posts.slice(firstPostIndex, lastPostIndex);
+
+      //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      //     return { posts: postsOnPage, hasNextPage } as any;
+      //   } catch (e) {
+      //     console.log("error in fetching");
+      //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      //     return { error: e as unknown as any };
+      //   }
+      // },
+      merge: (currentCache, newItems) => {
+        console.log("merge", { currentCache, newItems });
+        currentCache.hasNextPage = newItems.hasNextPage;
+        currentCache.posts.push(...newItems.posts);
+      },
+      forceRefetch({ currentArg, previousArg }) {
+        console.log("force refetch", { currentArg, previousArg });
+        return currentArg !== previousArg;
+      },
     }),
     getPostById: builder.query<PostDto, number>({
       query: (id) => `/${id}`,
     }),
   }),
 });
+
+//  mock BE call, allowing to pass page param and get 'hasNextPage' from BE
+//  need mock since JSONPlaceholder do not have this functionality
+// export const getPosts =  async  (page: number) {
+//   const posts = await fetchBaseQuery()
+// }
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
--- src/features/posts-list-connected/ui/posts-list-connected.tsx
+++ src/features/posts-list-connected/ui/posts-list-connected.tsx
@@ -1,26 +1,50 @@
 import { useVirtualizer } from "@tanstack/react-virtual";
 import { Post, postsApi } from "entities";
-import { useRef } from "react";
+import { debounce } from "lodash";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const PostsListConnected = () => {
   const parentRef = useRef<HTMLDivElement>(null);
-  const { data } = postsApi.useGetPostsQuery();
+  const [page, setPage] = useState(0);
+  const { data } = postsApi.useGetPostsQuery(page);
+
+  useEffect(() => console.log({ posts: data?.posts }), [data?.posts]);
+
+  // const count = data?.posts ? (data.hasNextPage ? data.posts.length + 1 : data.posts.length) : 0;
 
   const rowVirtualizer = useVirtualizer({
-    count: 100,
+    count: data ? (data.hasNextPage ? data.posts.length + 1 : data.posts.length) : 0,
     getScrollElement: () => parentRef.current,
     estimateSize: () => 35,
   });
 
-  const getPost = (index: number) => (data ? data[index] : undefined);
+  const increasePageDebounced = debounce(() => {
+    console.log("increasing page");
+    setPage((p) => p + 1);
+  });
+
+  useEffect(() => {
+    const [lastItem] = [...rowVirtualizer.getVirtualItems()].reverse();
+
+    if (!lastItem || !data || !data.posts || !data.hasNextPage) {
+      return;
+    }
+
+    if (lastItem.index >= data.posts.length - 1 && data.hasNextPage) {
+      increasePageDebounced();
+    }
+  }, [data, rowVirtualizer.getVirtualItems()]);
+
+  const getPost = (index: number) => (data?.posts ? data.posts[index] : undefined);
+  console.log({ posts: data?.posts });
 
   return (
     <>
       <div
         ref={parentRef}
         style={{
-          height: `400px`,
+          height: `250px`,
           overflow: "auto",
           paddingRight: 10,
         }}
