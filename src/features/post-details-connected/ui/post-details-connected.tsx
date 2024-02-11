a67e5576c9ebf0ebdbd749a2f807e409bd3076a5
bondarenko-scalara <e.bondarenko@scalara.de>
2024-02-11 19:52:11 +0300

    restructure dependencies and features

--- src/features/post-details-connected/ui/post-details-connected.tsx
+++ src/features/post-details-connected/ui/post-details-connected.tsx
@@ -1,6 +1,16 @@
 import { PostDetails, PostDto, postsApi } from "entities/";
+import { lastVisitedPostDetailsIdAtom } from "features/posts-list-connected/model/posts-virtial-scroll-atoms-store";
+import { useSetAtom } from "jotai";
+import { useEffect } from "react";
 
 export const PostDetailsConnected = ({ postId }: { postId: PostDto["id"] }) => {
   const { data } = postsApi.useGetPostByIdQuery(postId);
+  const setLastVisitedPostDetailsIndex = useSetAtom(lastVisitedPostDetailsIdAtom);
+
+  //  set postId in atom to scroll into that item, when we go back to the posts overview page
+  useEffect(() => {
+    setLastVisitedPostDetailsIndex(postId);
+  }, [postId, setLastVisitedPostDetailsIndex]);
+
   return <PostDetails post={data} />;
 };
--- src/pages/post-details-page/post-details-page.tsx
+++ src/pages/post-details-page/post-details-page.tsx
@@ -1,32 +1,21 @@
-import { PostDetailsConnected, lastVisitedPostDetailsIdAtom } from "features";
-import { useSetAtom } from "jotai";
+import { PostDetailsConnected } from "features";
 import { useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 export const PostDetailsPage = () => {
   const { postId } = useParams();
   const postIdNumber = Number(postId);
-  const setLastVisitedPostDetailsIndex = useSetAtom(lastVisitedPostDetailsIdAtom);
   const navigate = useNavigate();
 
-  //  set postId in atom to scroll into that item, when we go back to the posts overview page
+  //  check if given postId is actual number => redirect to root page if not
   useEffect(() => {
-    if (isFinite(postIdNumber)) {
-      setLastVisitedPostDetailsIndex(postIdNumber);
-    }
-  }, [postId, postIdNumber, setLastVisitedPostDetailsIndex]);
-
-  useEffect(() => {
-    if (!postId || !isFinite(postIdNumber)) {
-      console.warn('there is no postId, routing to the "/". Please check params definition');
-      navigate("/");
-    }
-  }, [postId, navigate, postIdNumber]);
+    isFinite(postIdNumber) ? null : navigate("/");
+  }, [navigate, postIdNumber]);
 
   return (
     <div>
       <Link to={"-1"}>На главную</Link>
-      {postId ? <PostDetailsConnected postId={postIdNumber}></PostDetailsConnected> : <div>Error</div>}
+      {postIdNumber ? <PostDetailsConnected postId={postIdNumber}></PostDetailsConnected> : <div>Error</div>}
     </div>
   );
 };
